e958b071dcce439bbb5dca8f3df418b175ce30f5
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-02-02 22:53:29 +0100

    add: eigth ex

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// Trova lo studente con id 2
+const student = students.find(student => student.id === 2);
+
+console.log(student);
